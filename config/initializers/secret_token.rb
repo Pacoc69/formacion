55b3e0a550304cb3df542bccd376620bd81cbf46
fcc <fcc@solumaticacanarias.es>
2012-01-23 13:21:04 +0000

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Formacion::Application.config.secret_token = 'e5050c43835396e536ecbd737120eae64659b3a01cc6f3c45ad95ff5ca76a8181ccb09c1667eed5434658a4f40b82f3864370cd714468cdf2a1e432010f1b24b'
